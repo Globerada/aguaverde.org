a26e863cc1ab97b900cf74abe4814a951f99b032
Manuel Regidor <mregidor90@gmail.com>
2018-10-19 16:26:39 +0200

    Deleting SVG for testing

--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -1,13 +1,11 @@
 <template>
   <nav class="main-nav flex gap-15">
 
-    <g-link to="/info/">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+    <g-link to="/info">
       <span>Información</span>
     </g-link>
 
     <g-link to="/blog">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-radio"><circle cx="12" cy="12" r="2"></circle><path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14"></path></svg>
       <span>Blog</span>
     </g-link>
   </nav>
