4720807bd0a5137bbaac3abc1b3fcd59d35cf636
Manuel Regidor <mregidor90@gmail.com>
2018-10-20 16:27:40 +0200

    Added ImageModal

---
+++ src/components/modals/ImageModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <AppModal name="image" :image="true">
+      <template slot="body" slot-scope="{ params }">
+        <img :src="params.image" />
+      </template>
+    </AppModal>
+  </div>
+</template>
+
+<script>
+import AppModal from '../AppModal'
+export default {
+  components: {
+    AppModal
+  }  
+}
+</script>
+
+<style lang="scss">
+.app-modal-inner--image {
+  img {
+    margin-bottom: 0;
+  }
+}
+</style>
