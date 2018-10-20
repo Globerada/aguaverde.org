1bad4bcc2731581bcb33d5f9855f24e3eaa72b21
Manuel Regidor <mregidor90@gmail.com>
2018-10-20 02:30:25 +0200

    Carousl not working SSR

--- src/components/home/HomeCarousel.vue
+++ src/components/home/HomeCarousel.vue
@@ -1,28 +1,30 @@
 <template>
   <Section :dots="false" primary="true" style="padding-bottom: 0px;">
     <div class="container text-center">      
-      <h2>¡Mira qué bien nos lo pasamos!</h2>      
-      <Carousel 
-        :per-page="1"
-        :min-swipe-distance="48"
-        :pagination-enabled="true"
-        :pagination-padding="12"
-        pagination-active-color="#f8188e"
-        pagination-color="#c9c9c9"
-        :pagination-size="8"
-        :navigation-enabled="true"
-        ref="carousel"
-      >
-        <Slide><g-image width="760" src="~/assets/images/carousel/1.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/2.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/3.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/4.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/5.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/6.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/7.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/8.jpg" /></Slide>
-        <Slide><g-image width="760" src="~/assets/images/carousel/9.jpg" /></Slide>
-      </Carousel>
+      <h2>¡Mira qué bien nos lo pasamos!</h2>
+      <div v-if="showCarousel">
+        <Carousel 
+          :per-page="1"
+          :min-swipe-distance="48"
+          :pagination-enabled="true"
+          :pagination-padding="12"
+          pagination-active-color="#f8188e"
+          pagination-color="#c9c9c9"
+          :pagination-size="8"
+          :navigation-enabled="true"
+          ref="carousel"
+        >
+          <Slide><g-image width="760" src="~/assets/images/carousel/1.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/2.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/3.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/4.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/5.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/6.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/7.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/8.jpg" /></Slide>
+          <Slide><g-image width="760" src="~/assets/images/carousel/9.jpg" /></Slide>
+        </Carousel>
+      </div>
     </div>
   </Section>
 </template>
@@ -31,14 +33,16 @@
 import { Carousel, Slide } from 'vue-carousel'
 export default {
   components: {
-  Carousel,
-  Slide,
+    Carousel,
+    Slide,
   },
-
   data() {
     return {
-      tab: 'graphql'
+      showCarousel : false
     }
+  },
+  mounted() {
+    this.showCarousel = true
   }
 }
 
